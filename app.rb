88371b14415e38fdb938cce5c1f53bccd88a3a3e
J. Singleton <jennifer.singleton@gmail.com>
2019-04-11 02:57:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     @name = params[:team_name]
     @coach = params[:coach]
     @pg = params[:pg]
-    @shooting_guard = params[:shooting_guard]
+    @sg = params[:sg]
     @small_forward = params[:small_forward]
     @power_forward = params[:power_forward]
     @center = params[:center]
